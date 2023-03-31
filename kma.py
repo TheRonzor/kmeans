dc761a624d37a9e0b40d9b68dd85fc5bca816ab8
Ron <ronniecougar@gmail.com>
2023-03-30 22:23:57 -0400

    first working version

--- kma.py
+++ kma.py
@@ -18,118 +18,128 @@ plt.style.use('dark_background')
 __version__ = '0.0.1'
 
 class KMeansAnim():
-    FIG_SIZE    = (4,4)
-    ALPHA_DATA  = 0.9
-    ALPHA_CENT  = 0.6
+    FIG_SIZE    = (8,8)
+    ALPHA_DATA  = 0.6
+    ALPHA_CENT  = 0.9
     SIZE_DATA   = 20
     SIZE_CENT   = 100
-    DT = 0.2
+
+    DT          = 1
 
     def __init__(self, 
-                 n_points           = 100, 
-                 n_clusters         = 3, 
+                 n_points           = 1000, 
+                 n_clusters         = 12, 
                  n_clusters_guess   = None, 
+                 method             = 'naive',
                  seed               = 42):
         
-        # Initialize PRNG
-        self.seed = seed
-        self.rng = np.random.RandomState(self.seed)
-        
-        # Initialize data
+        # Initialize settings
         self.n_points   = n_points
         self.n_clusters = n_clusters
-        
-        # Default guess is the clairvoyant one, since my AI is the best AI ;-)
         if n_clusters_guess is None:
+            # Default guess is the clairvoyant one, since my AI is the best AI ;-)
             self.n_clusters_guess = self.n_clusters
         else:
             self.n_clusters_guess = n_clusters_guess
-        
+        self.seed = seed
+        self.rng = np.random.RandomState(self.seed)
+
         # Create the data
         self.data, self.y = mb(n_samples    = self.n_points, 
                                centers      = self.n_clusters, 
                                n_features   = 2,  
                                random_state = self.rng)
-        
-        
-        # Rescale data to be in the unit square
-        self.data = MMS().fit_transform(self.data)
+        self.data = MMS().fit_transform(self.data) # Rescale data to be in the unit square
 
         # Create the centroids
-        self.CreateCentroids()
-
-        # Combine data and centroids into a single arrray
-        self.X = np.concatenate([self.data, self.centroids])
-
-        # Color all the points white at first
-        self.color_arr = np.ones([len(self.X),4])
-        self.color_arr[:,-1] = self.ALPHA_DATA
-        
-        # Set the colors for the centroids, but make them transparant (opaque for testing)
-        self.color_arr[self.n_points:, :-1] = self.cluster_colors
-        self.color_arr[self.n_points:, -1] = self.ALPHA_CENT
-        
-        # Set the sizes for data and centroids
-        self.size_arr = [self.SIZE_DATA]*self.n_points + [self.SIZE_CENT]*self.n_clusters_guess
-
-        # Initialize the figure
-        self.fig, self.ax = plt.subplots(figsize=KMeansAnim.FIG_SIZE)
-        self.scat = plt.scatter(self.X[:,0], self.X[:,1], ec='k')
-        self.ax.set_xticks([])
-        self.ax.set_yticks([])
-        self.ax.set_xlim(-0.1,1.1)
-        self.ax.set_ylim(-0.1,1.1)
-        
-        self.scat.set_color(self.color_arr)
-        self.scat.set_sizes(self.size_arr)
-
-        return
-    
-    def CreateCentroids(self, method='from_data'):
         if method == 'naive':
             self.centroids = self.rng.random(size=(self.n_clusters, 2))
         elif method == 'from_data':
             self.centroids = self.data[self.rng.choice(self.data.shape[0], replace=False, size=self.n_clusters_guess)]
         elif method == 'k++':
             raise(NotImplementedError())
-        self.cluster_colors = get_cmap('rainbow')(np.linspace(0, 1, self.n_clusters_guess))[:,:-1]
-        print(self.cluster_colors)
-        return 
+        self.cluster_colors = get_cmap('rainbow')(np.linspace(0, 1, self.n_clusters_guess))[:,:-1] # Everything except the alpha
+
+        # Initialize the figure objects
+        self.fig, self.ax = plt.subplots(figsize=self.FIG_SIZE)
+
+        self.scat_data = plt.scatter([], [], ec='k', alpha = self.ALPHA_DATA, s = self.SIZE_DATA)
+        self.scat_cent = plt.scatter([], [], ec='w', s = self.SIZE_CENT, marker='*', linewidths=1)
+
+        # Initialize the color arrays
+        self.color_data = np.ones([len(self.data),3])
+        self.color_cent = self.cluster_colors
+
+        # Make the centroids invisible for now
+        self.scat_cent.set_alpha(0)
+
+        self.ax.set_xticks([])
+        self.ax.set_yticks([])
+        self.ax.set_xlim(-0.01,1.01)
+        self.ax.set_ylim(-0.01,1.01)
+        self.ax.set_title('Initializing...')
+        return
 
     def ShowInitialData(self):
-        print('Called ShowInitialData')
+        # Nothing here for now
         sleep(self.DT)
-        print('Exit ShowInitialData')
-        return self.scat,
+        return self.scat_data, self.scat_cent,
 
-    def UpdateClusterLabels(self):
+    def UpdateClusters(self):
         distances = []
         for c in self.centroids:
             d = np.sum((self.data - c)**2,axis=1)
             distances.append(d)
         self.labels = np.argmin(np.array(distances).T,axis=1)
+
+        for label in np.unique(self.labels):
+            idx = np.where(self.labels == label)
+            self.color_data[idx, :] = self.cluster_colors[label, :]
+        return
+    
+    def UpdateCentroids(self):
+        for i in range(self.n_clusters_guess):
+            idx = self.labels == i
+            self.centroids[i,:] = np.mean(self.data[idx,:], axis=0)
         return
 
     def Update(self, frame_number):
         if frame_number == 0:
-            print('f1')
-            self.UpdateClusterLabels()
+            self.scat_cent.set_facecolor(self.color_cent)
+            self.scat_cent.set_alpha(self.ALPHA_CENT)
+            sleep(self.DT)
         elif frame_number == 1:
-            print('f2')
-            self.X += 0.01
-            self.X %= 1
+            self.UpdateClusters()
+            sleep(self.DT)
         else:
-            print('fN')
-            self.X += 0.01
-            self.X %= 1
-        self.scat.set_offsets(self.X)
-        return self.scat,
+            self.UpdateClusters()
+            self.UpdateCentroids()
+            sleep(self.DT)
+        
+        #
+        #
+        #
+        #self.data[:self.n_points, :] += np.random.normal(size=self.data.shape, scale=0.01) + 0.001
+        #self.data[:self.n_points, :] %= 1
+        #
+        #
+        #
+
+        self.ax.set_title('Frame ' + str(frame_number))
+
+        self.scat_data.set_offsets(self.data)
+        self.scat_data.set_color(self.color_data)
+
+        self.scat_cent.set_offsets(self.centroids)
+        #self.scat_cent.set_color(self.color_cent)
+        return self.scat_data, self.scat_cent, 
 
     def Go(self):
         self.ani = FuncAnimation(self.fig, 
                                  self.Update,
-                                 blit=False, 
-                                 interval=1000,
+                                 init_func = self.ShowInitialData,
+                                 blit=False,  # Can't seem to get blitting to work with titles, even if using ax.text inside the plot area :-(
+                                 interval=33,
                                  cache_frame_data=True)
         plt.show()
         return
